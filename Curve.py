516de1aee4c68bf9f17f464aae740f15badecdd8
MrCake4 <winstonjreichelt@gmail.com>
2026-03-05 15:28:07 +0100

    Multiple rmls now get created. Plot not working yet.

--- Curve.py
+++ Curve.py
@@ -4,20 +4,18 @@ import plotly.graph_objects as go
 from HistogramData import HistogramData
 # endregion
 
-class Histogram:
-    """
-    A 2DHistogram Curve to handle all the plotting logic (f.E. the reflectivity curve) 
-    Plots are plotted using Plotly.
-    """
 
-    # TODO: This class is similair to the Histogram class, maybe use inheritance
+class Curve:
+    """
+    A curve plot (e.g. reflectivity vs photon energy).
+    Uses Plotly for interactive visualization.
+    """
 
     def __init__(self, dataX, dataY, xLabel="No label", yLabel="No label", title="No title"):
 
-        self.curveDataX = dataX
-        self.curveDataY = dataY
+        self.curveDataX = np.array(dataX)
+        self.curveDataY = np.array(dataY)
 
-        # Histogram customization
         self.xlabel = xLabel
         self.ylabel = yLabel
         self.title = title
@@ -26,186 +24,44 @@ class Histogram:
 
     def GetPlotHTML(self) -> str:
         """
-        Returns interactive Plotly HTML.
-        It assumes that this plot is a 2D histogram and constructs a 2D histogram.
+        Returns interactive Plotly HTML for a curve plot.
         """
 
-        dataX = np.asarray(self.curveDataX.data)
-        dataY = np.asarray(self.curveDataY.data)
+        # Create figure
+        fig = go.Figure()
 
-        # region Plot
-        # Create subplots
-        fig = make_subplots(
-            rows=2, cols=2,
-            column_widths=[0.15, 0.85],  # main vs side
-            row_heights=[0.85, 0.15],    # main vs bottom
-            specs=[
-                [{"type": "histogram"}, {"type": "histogram2d"}],  # side + main 2d histogram
-                [None, {"type": "histogram"}]                      # bottom histogram
-            ],
-            horizontal_spacing=0.02,
-            vertical_spacing=0.02
-        )
-        # Link x of bottom histogram to main histogram
-        fig.update_xaxes(
-            matches='x2', 
-            row=2, col=2, 
-            title_text=self.xlabel)
-
-        # Link y of side histogram to main histogram
-        fig.update_yaxes(
-            matches='y2',
-            row=1, col=1, 
-            title_text=self.ylabel
+        # Add reflectivity curve
+        fig.add_trace(
+            go.Scatter(
+                x=self.curveDataX,
+                y=self.curveDataY,
+                mode="lines+markers",
+                name="Reflectivity"
+            )
         )
 
-        # Remove numbers on main histogram
-        fig.update_xaxes(showticklabels=False, ticks="outside", ticklen=6, row=1, col=2)
-        fig.update_yaxes(showticklabels=False, ticks="outside", ticklen=6, row=1, col=2)
-
-        # Main 2D histogram
-        fig.add_trace(go.Histogram2d(x=dataX, y=dataY, nbinsx=bins[0], nbinsy=bins[1],
-                                    colorscale="Viridis"), row=1, col=2)
-
-        # Side histogram (Y marginal)
-        fig.add_trace(go.Histogram(
-            y=dataY, 
-            ybins=dict(
-                start=edgesY[0],
-                end=edgesY[-1],
-                size=edgesY[1] - edgesY[0]
-            ),
-            marker_color="cornflowerblue"), 
-            row=1, col=1)
-
-        # Bottom histogram (X marginal)
-        fig.add_trace(go.Histogram(
-            x=dataX, 
-            xbins=dict(
-                start=edgesX[0],
-                end=edgesX[-1],
-                size=edgesX[1] - edgesX[0]
-            ),
-            marker_color="cornflowerblue"), row=2, col=2)
-
-        
-        # Center of Mass line Side histogram
-        fig.add_shape(
-            type="line",
-            x0=0,
-            y0=self.curveDataY.info["centerOfMass"],
-            x1=self.curveDataY.info["y"],
-            y1=self.curveDataY.info["centerOfMass"],
-            line=dict(color="green", width=2),
-            xref="x1",
-            yref="y1"
-        )
-
-        # Center of Mass line Bottom histogram
-        fig.add_shape(
-            type="line",
-            x0=self.curveDataX.info["centerOfMass"],
-            y0=0,
-            x1=self.curveDataX.info["centerOfMass"],
-            y1=self.curveDataX.info["y"],
-            line=dict(color="green", width=2),
-            xref="x3",
-            yref="y3"
-        )
-
-        # FWHM lines side histogram
-        fig.add_shape(
-            type="line",
-            x0=self.curveDataY.info["y"],
-            y0=self.curveDataY.info["x1"],
-            x1=self.curveDataY.info["y"],
-            y1=self.curveDataY.info["x2"],
-            line=dict(color="orange", width=2),
-            xref="x1",
-            yref="y1"
-        )
-
-        fig.add_shape(
-            type="line",
-            x0=0,
-            y0=self.curveDataY.info["x1"],
-            x1=self.curveDataY.info["y"],
-            y1=self.curveDataY.info["x1"],
-            line=dict(color="orange", width=2),
-            xref="x1",
-            yref="y1"
-        )
-
-        fig.add_shape(
-            type="line",
-            x0=0,
-            y0=self.curveDataY.info["x2"],
-            x1=self.curveDataY.info["y"],
-            y1=self.curveDataY.info["x2"],
-            line=dict(color="orange", width=2),
-            xref="x1",
-            yref="y1"
-        )
-
-        
-
-        # FWHM lines bottom histogram
-        fig.add_shape(
-            type="line",
-            x0=self.curveDataX.info["x1"],
-            y0=self.curveDataX.info["y"],
-            x1=self.curveDataX.info["x2"],
-            y1=self.curveDataX.info["y"],
-            line=dict(color="orange", width=2),
-            xref="x3",
-            yref="y3"
-        )
-
-        fig.add_shape(
-            type="line",
-            x0=self.curveDataX.info["x1"],
-            y0=0,
-            x1=self.curveDataX.info["x1"],
-            y1=self.curveDataX.info["y"],
-            line=dict(color="orange", width=2),
-            xref="x3",
-            yref="y3"
-        )
-
-        fig.add_shape(
-            type="line",
-            x0=self.curveDataX.info["x2"],
-            y0=0,
-            x1=self.curveDataX.info["x2"],
-            y1=self.curveDataX.info["y"],
-            line=dict(color="orange", width=2),
-            xref="x3",
-            yref="y3"
-        )        
-
-        # Update layout
+        # Layout
         fig.update_layout(
             height=800,
             width=800,
             title=self.title,
-            showlegend=False,
-            bargap=0.05
+            xaxis_title=self.xlabel,
+            yaxis_title=self.ylabel,
+            showlegend=False
         )
-        # endregion
 
-        # region controls
+        # Controls
         fig.update_layout(
             dragmode="pan"
         )
-        # endregion
 
-        # Return embeddable HTML
+        # Return HTML
         return fig.to_html(
-            full_html=False, 
+            full_html=False,
             include_plotlyjs="cdn",
             config={
-                "scrollZoom": True,      # wheel zoom
-                "doubleClick": "reset",  # double-click resets
-                "displaylogo": False     # remove Plotly logo
+                "scrollZoom": True,
+                "doubleClick": "reset",
+                "displaylogo": False
             }
         )
--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 from flask import Flask, render_template, request, send_file, redirect, flash, url_for
 from FileOperations import *
 import rayx, os, subprocess, traceback, io, base64, time
+from rml import *
 from Histogram import Histogram
 from Curve import Curve
+from pathlib import Path
 import pandas as pd
 import numpy as np
 from werkzeug.utils import secure_filename
@@ -12,8 +14,8 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # Upload- & Output folder, creates one if it doesn't exist
-UPLOAD_FOLDER = "./uploads/"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+UPLOAD_FOLDER = Path("./uploads/")
+UPLOAD_FOLDER.mkdir(exist_ok=True)
 
 output_file_name = ""
 
@@ -147,76 +149,100 @@ def handle_post_reflectivity():
         # Check if file is allowed and save it, if it is
         if rml_file and allowed_file(rml_file.filename):
             filename = secure_filename(rml_file.filename)
-            rml_file.filename = filename
-            save_file(UPLOAD_FOLDER, rml_file)
+            path = os.path.join(UPLOAD_FOLDER, filename)
+
+            rml_file.save(path)
+
+            generate_energy_rmls(path, UPLOAD_FOLDER)
 
         output_file_name = rml_file.filename
+
+        # Dataframe to hold the electric field strength (eV) for each element, used to plot the reflectivity curve
+        columns = ["eV", "reflectivity"]
+        electric_fields = pd.DataFrame(columns=columns)
         
-        try:
-            # Trace beamline    
-            beamline = get_beamline(rml_file)
-            traced_beamline = beamline.trace()
+        path_to_energy_scan = os.path.join(UPLOAD_FOLDER, "energy_scan/")
 
-            # Create pandas dataframe
-            columns = [
-                "direction_x", "direction_y", "direction_z",
-                "electric_field_x", "electric_field_y", "electric_field_z",
-                "energy", "event_type",
-                "last_element_id", "order", "path_length",
-                "position_x", "position_y", "position_z",
-                "ray_id", "source_id",
-            ]
-
-            df = pd.DataFrame({col: getattr(traced_beamline, col) for col in columns})
-
-            # Remove file from server
-            remove_file(UPLOAD_FOLDER, rml_file)
-
-            # =====================
-            # identical until here
-
-            # Plot the traced beamline
-            last_element = df["last_element_id"]
-
-            # Array to hold the electric field strength (eV) for each element, used to plot the reflectivity curve
-            electric_fields = []
-
+        # Loop through the generated rml files
+        for rml in os.listdir(path_to_energy_scan):
             
-            # TODO: Put Below in an for each loop for every rml. This needs to happen to each rml
-            electric_field_source = 0
-            electric_field_mirror = 0
-            
-            # Get the electric field strength for the source
-            for source in range(len(beamline.sources)):
-                mask = last_element == source
-                
-                electric_field_source = get_n_electric_field(df[mask])
+            try:
+                # Trace beamline    
+                beamline = rayx.import_beamline(os.path.join(path_to_energy_scan, rml))
+                traced_beamline = beamline.trace()
 
-            # If the beamline has only one element or more than two, redirect to avoid errors
-            if len(beamline.elements) <= 1 or len(beamline.elements) > 2:
-                print("Beamline has only one element or more than two, redirecting to home page.")
-                redirect(url_for("index"))
-            else:
-                index = 1
-                try:
-                    # Get the electric field strength for the mirror
-                    for element in range(len(beamline.elements)):
-                        mask = last_element == element + len(beamline.sources)
+                # Create pandas dataframe
+                columns = [
+                    "direction_x", "direction_y", "direction_z",
+                    "electric_field_x", "electric_field_y", "electric_field_z",
+                    "energy", "event_type",
+                    "last_element_id", "order", "path_length",
+                    "position_x", "position_y", "position_z",
+                    "ray_id", "source_id",
+                ]
 
-                        electric_field_mirror = get_n_electric_field(df[mask])
-                except:
-                    print("Index out of range" + str(index))
-                    pass
-            
-            # Calculate the reflectivity by dividing the electric field strength of the mirror by that of the source
-            reflectivity = electric_field_mirror / electric_field_source
+                df = pd.DataFrame({col: getattr(traced_beamline, col) for col in columns})
 
-        except Exception as e:
-            traceback.print_exc()
-            return render_template("displayPy.html", exception=e)
+                # Remove file from server
+                # remove_file(path_to_energy_scan, rml)
+
+                # =====================
+                # identical until here
+
+                # Plot the traced beamline
+                last_element = df["last_element_id"]
+
+
+                # TODO: Put Below in an for each loop for every rml. This needs to happen to each rml
+                electric_field_source = 0
+                electric_field_mirror = 0
+
+                # Get the electric field strength for the source
+                for source in range(len(beamline.sources)):
+                    mask = last_element == source
+
+                    electric_field_source = get_n_electric_field(df[mask])
+
+                # If the beamline has only one element or more than two, redirect to avoid errors
+                if len(beamline.elements) <= 1 or len(beamline.elements) > 2:
+                    print("Beamline has only one element or more than two, redirecting to home page.")
+                    redirect(url_for("index"))
+                else:
+                    index = 1
+                    try:
+                        # Get the electric field strength for the mirror
+                        for element in range(len(beamline.elements)):
+                            mask = last_element == element + len(beamline.sources)
+
+                            electric_field_mirror = get_n_electric_field(df[mask])
+                    except:
+                        print("Index out of range" + str(index))
+                        pass
+                    
+                # Calculate the reflectivity by dividing the electric field strength of the mirror by that of the source
+                reflectivity = np.abs(electric_field_mirror / electric_field_source)
+
+                # Add the electric field strength and reflectivity to the dataframe
+                electric_fields = pd.concat(
+                    [
+                        electric_fields,
+                        pd.DataFrame([{
+                            "eV": beamline.sources[0].energy,
+                            "reflectivity": reflectivity
+                        }])
+                    ],
+                    ignore_index=True
+                )
+
+                # Plot the reflectivity curve
+                plot_data = Curve(electric_fields["eV"], electric_fields["reflectivity"], xLabel="Energy (eV)", yLabel="Reflectivity", title="Reflectivity Curve").GetPlotHTML()
+
+            except Exception as e:
+                traceback.print_exc()
+                return render_template("displayPy.html", exception=e)
       
     return render_template(
-        "displayPy.html", 
+        "reflectivity.html", 
         RMLFileName=get_cleaned_filename(output_file_name), 
         plot_data=plot_data,
     )
